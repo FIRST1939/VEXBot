5518cebc13034e9f857e0df43fde62c9ad29ab3c
Drew-Childs <60235260+Drew-Childs@users.noreply.github.com>
2023-11-04 10:10:39 -0500

    Updated constants

--- src/constants.h
+++ src/constants.h
@@ -1,9 +1,9 @@
 //Motor declarations for names
 pros::Motor LF (20);
-pros::Motor RF (19);
-pros::Motor LM (18);
-pros::Motor RM (17);
-pros::Motor LR (16);
-pros::Motor RR (15);
+pros::Motor RF (16);
+pros::Motor LM (17);
+pros::Motor RM (15);
+pros::Motor LR (19);
+pros::Motor RR (18);
 
-pros::Motor CATAPULT (7);
+pros::Motor CATAPULT (10);
