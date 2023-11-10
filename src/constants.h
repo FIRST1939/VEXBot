0e551eaa38c23eaa5c4099e6b2002742c3576f80
Drew-Childs <60235260+Drew-Childs@users.noreply.github.com>
2023-11-09 21:33:17 -0600

    Why does this language exist?

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,19 @@
-//Motor declarations for names
-pros::Motor LeftFront (18);
+// Declarations for drivetrain motors
+pros::Motor LeftFront (20);
+pros::Motor RightFront (11);
 pros::Motor LeftMiddle (19);
-pros::Motor LeftRear (20);
-pros::Motor RightFront (8);
-pros::Motor RightMiddle (9);
-pros::Motor RightRear (10);
+pros::Motor RightMiddle (13);
+pros::Motor LeftRear (19);
+pros::Motor RightRear (18);
+
+pros::Motor Flywheel (1);
+pros::Motor Intake (2);
+
+// Controllers
+pros::Controller controller(pros::E_CONTROLLER_MASTER);
+
+// Handy Enums
+enum DriveType { ARCADE, TANK };
+
+// Gyro
+pros::IMU inertial(10);
