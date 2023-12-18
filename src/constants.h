376a6691634ff1a103c279258689c676b36d6dfe
Drew-Childs <60235260+Drew-Childs@users.noreply.github.com>
2023-12-18 11:43:03 -0600

    Update constants.h

--- src/constants.h
+++ src/constants.h
@@ -5,11 +5,11 @@ pros::Motor LeftFront (20);
 pros::Motor RightFront (11);
 pros::Motor LeftMiddle (19);
 pros::Motor RightMiddle (13);
-pros::Motor LeftRear (19);
-pros::Motor RightRear (18);
+pros::Motor LeftRear (18);
+pros::Motor RightRear (14);
 
 pros::Motor Flywheel (1);
-pros::Motor Intake (2);
+pros::Motor Intake (9);
 
 // Controllers
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
@@ -18,6 +18,8 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 enum DriveType { ARCADE, TANK };
 
 // Gyro
-pros::IMU inertial(10);
+pros::IMU inertial(8);
 
-// TODO add pneumatic controlls here
+// Pneumatic controls here
+pros::ADIDigitalOut piston1 (1);
+pros::ADIDigitalOut piston2 (2);
